cb02ecdbf2683b6fea0a3c0e70b0329783ee7d46
Olga Rosas <olga.rosas@gmail.com>
2019-06-03 18:48:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 string = "Hello there, and how are you?"
 def reverse_each_word(string)
-  new_string = string.split
-  new_string.each do |words|
-  puts words.reverse
+  new_array = string.split
+  new_array.each do |words|
+    words.reverse
   end
 end
